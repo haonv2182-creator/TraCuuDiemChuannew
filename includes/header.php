9d48e310b1cb904c5c1ec7c1d0264a6e4c1060f8
VanHao <haonv2182@ut.edu.vn>
2026-06-30 22:40:06 +0700

    fs

--- includes/header.php
+++ includes/header.php
@@ -25,7 +25,9 @@ $styleVersion = file_exists($styleFile)
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="<?= url('assets/css/style.css') ?>?v=<?= $styleVersion ?>">
+
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@300;400;500;600;700&display=swap">
+  <link rel="stylesheet" href="<?= url('assets/css/unified.css') ?>?v=<?= time() ?>">
 </head>
 
 <body>
